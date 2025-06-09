20747ed267aed3d6190951526abfdf62b8be9c3d
justslick23 <tokelo.foso@cbs.co.ls>
2025-06-09 19:46:30 +0200

    commit

--- resources/views/invoices/pdf.blade.php
+++ resources/views/invoices/pdf.blade.php
@@ -13,7 +13,7 @@
         }
 
         body {
-            font-family: 'Inter', Arial, sans-serif;
+            font-family: 'Helvetica', Arial, sans-serif;
             font-size: 14px;
             line-height: 1.4;
             color: #333333;
